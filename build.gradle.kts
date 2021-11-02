b93be6d48c4add4d3c2f828fdd9ae891015da2bf
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2021-11-02 09:49:40 +0000

    ESWE-196 security update and CP ingress (#8)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.8"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.12"
   kotlin("plugin.spring") version "1.5.31"
 }
 
@@ -14,12 +14,12 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("io.springfox:springfox-boot-starter:3.0.0")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.4")
+  implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.5")
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:junit-jupiter:1.16.0")
+  testImplementation("org.testcontainers:junit-jupiter:1.16.2")
   testImplementation("io.projectreactor:reactor-test")
 }
 
